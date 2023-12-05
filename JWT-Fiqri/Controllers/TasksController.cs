ef32dd93e0ddf99d73c9903bb1362c10e67d4e36
PathumLD <2020is031@stu.ucsc.cmb.ac.lk>
2023-12-05 14:43:27 +0530

    Add project files.

---
+++ JWT-Fiqri/Controllers/TasksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT_Fiqri.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TasksController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult Tasks()
+		{
+			var tasks = new string[] { "Task1", "Taask2", "Task3" };
+			return Ok();
+		}
+	}
+}
